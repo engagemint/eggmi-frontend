5dd15113896402d75f6ba208ac1b244c09c023cd
Abraham Huerta <abrhuerta@gmail.com>
2022-03-09 18:51:52 -0600

    initial commit

---
+++ src/views/Homepage.jsx
@@ -0,0 +1,194 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <>
+      <section className='top-section'>
+        <h1 className='Egmi'>Everyone's Gonna Make It</h1>
+        <img
+          className='background-image'
+          src='images/solarpunk.jpeg'
+          alt='solarpunk aesthetic'
+        />
+        <h1 className='welcome-text'>welcome</h1>
+      </section>
+      <section className='middle-section'>
+        <div class='profile'>
+          <h2>We're Engagemint, a Local Civic Participation Holostructure</h2>
+        </div>{" "}
+        <hr />
+        <section class='about-us'>
+          <h2>About Us</h2>
+          <div class='quote-row'>
+            <a
+              class='title-link'
+              href='https://mirror.xyz/myceliastargaze.eth/ZW6hYQ9QBv3REmprXu1wIbGUxm5xXqLhuzFiCvndw-k'>
+              <h3>What is Engagemint?</h3>
+            </a>
+            <div class='blockquote-left'>
+              <blockquote cite='https://mirror.xyz/myceliastargaze.eth/ZW6hYQ9QBv3REmprXu1wIbGUxm5xXqLhuzFiCvndw-k'>
+                <p>
+                  {" "}
+                  Engagemint is a peer-to-peer legitimacy protocol, where
+                  residents reward others with Civic Digital Native Assets{" "}
+                  <br />
+                  This gives users ownership over their reputation and
+                  sovereignty over their network
+                </p>
+                <p>
+                  A{" "}
+                  <a
+                    class='inquote-link'
+                    href='https://mirror.xyz/myceliastargaze.eth/n0T3ZmiNC1nMbMGvrocMYvMBKyiXgeS3KHOSpzIVj0E'>
+                    Civic Participation
+                  </a>{" "}
+                  <a
+                    class='inquote-link'
+                    href='https://mirror.xyz/myceliastargaze.eth/ZW6hYQ9QBv3REmprXu1wIbGUxm5xXqLhuzFiCvndw-k'>
+                    Holostrusture
+                  </a>
+                  ,{" "}
+                  <a
+                    class='inquote-link'
+                    href='https://twitter.com/Engagemint_eth'>
+                    Engagemint
+                  </a>{" "}
+                  encourages flourishing participation in city proceedings.
+                  Establishing legitimacy-networks of beneficial, influential,
+                  and trustworthy local community members, Engagemint increases
+                  trust and facilitates sense-making. Through providing
+                  augmented and improved avenues for civic activism, residents
+                  access a larger role in improving their city infrastructure:
+                  from Transit Accessibility to Housing Density
+                  <p>
+                    The downstream carbon reduction effects from these types of
+                    measures are necessary to address our climate catastrophe
+                  </p>
+                </p>
+              </blockquote>
+            </div>
+          </div>
+          <div class='quote-row'>
+            <a
+              class='title-link'
+              href='https://mirror.xyz/myceliastargaze.eth/ZW6hYQ9QBv3REmprXu1wIbGUxm5xXqLhuzFiCvndw-k'>
+              <h3>Our Mission</h3>
+            </a>
+            <div class='blockquote-right'>
+              <blockquote cite='https://twitter.com/Engagemint_eth/status/1500881587004755974?s=20&t=LPsLkyYdWaVetf3GLQpiMA'>
+                <p>
+                  Engagemint is a small step towards making government web3,
+                  starting at the local level
+                </p>
+                <p>
+                  We're showcasing the potential for reputation systems, based
+                  on earned legitimacy, through{" "}
+                  <a class='inquote-link' href='https://twitter.com/GovrnHQ'>
+                    @GovrnHQ
+                  </a>
+                  ,
+                  <a
+                    class='inquote-link'
+                    href='https://twitter.com/mintkudosXYZ'>
+                    @mintkudosXYZ
+                  </a>
+                  , and a{" "}
+                  <a
+                    class='inquote-link'
+                    href='https://twitter.com/LensProtocol'>
+                    @LensProtocol
+                  </a>
+                  -like composable social graph
+                </p>
+                <p>
+                  We're a{" "}
+                  <a class='inquote-link' href='https://twitter.com/_KaliDAO'>
+                    @_KaliDAO
+                  </a>{" "}
+                  Wyoming LLC
+                </p>
+              </blockquote>
+            </div>
+          </div>
+        </section>
+        <section class='quote-row'>
+          <h3>Gitcoin Grant Round #13</h3>
+          <div class='blockquote-left'>
+            <p>
+              We're excited to announce our partnership with Gitcoin in their
+              13th Grant Round <br />
+              Find us at:{" "}
+              <a
+                class='inquote-link'
+                href='https://gitcoin.co/grants/4943/engagemint-civic-legitimacy-participation-holostru'>
+                https://gitcoin.co/grants/4943/engagemint-civic-legitimacy-participation-holostru
+              </a>
+            </p>
+            <p>
+              Thanks to quadratic funding, all contributions are magnified by
+              several factors <br />
+              The magnitude of your impact if outsized relative to direct
+              donations (which are still accepted at engagemint.eth :D)
+            </p>
+          </div>
+        </section>
+        <section class='quote-row'>
+          <h3>Getting Involved</h3>
+          <div class='blockquote-right'>
+            <p>We're looking for participation from:</p>
+            <p>🎨 Artists</p>
+            <p>🏗️ Builders</p>
+            <p>💻 Coders</p>
+            <p>⿻ Designers</p>
+            <p>📖 Educators</p>
+            <p>🌬️ Free-Spirits</p>
+            <p>
+              Please find us in our Discord:{" "}
+              <a class='inquote-link' href='https://discord.gg/uNmJ8xmP'>
+                https://discord.gg/uNmJ8xmP
+              </a>
+            </p>
+          </div>
+        </section>
+        <section class='quote-row'>
+          <h3>Why Engage?</h3>
+          <div class='blockquote-left'>
+            <p>
+              Building these proof-of-legitimacy networks facilitates
+              accountability, from personal relationships to electoral
+              decisions. This is a minuscule step towards onchain identities,
+              but an important bridge between chainspace and meatdimension.
+            </p>
+            <p>
+              Focus for this project isn’t to more crypto-native tooling ––
+              enough of that exists already. Engagemint is a gentle introduction
+              for everyday users to showcase the multitude of benefits that our
+              new tech bring us
+            </p>
+          </div>
+          <hr />
+          <div class='contact-us'>
+            <h2>Get In Touch</h2>
+          </div>
+        </section>
+      </section>
+      <div class='bottom-container'>
+        <a class='footer-link' href='https://github.com/myceliaStargaze/egmi'>
+          GitHub Repo
+        </a>
+        <a
+          class='footer-link'
+          href='https://mirror.xyz/myceliastargaze.eth/ZW6hYQ9QBv3REmprXu1wIbGUxm5xXqLhuzFiCvndw-k'>
+          Mirror.xyz
+        </a>
+        <a class='footer-link' href='https://twitter.com/engagemint_eth'>
+          Twitter
+        </a>
+
+        <p class='copyright'>© Engagemint.</p>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
